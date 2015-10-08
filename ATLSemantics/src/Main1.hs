69a348863611ab88d0119599504b5d2a110070c4
Rémi Douence <douence@dhcp-2-77.rech172-28.emn.fr>
2015-10-08 17:21:28 +0200

    strict and lazy semantics by massimo and remi

---
+++ ATLSemantics/src/Main1.hs
@@ -0,0 +1,135 @@
+module Main where
+
+--main::IO()
+--main = putStrLn (show (transformation m0))
+
+type Identifier = Int
+              
+type Element = (Identifier,Type)
+
+type Set a = [a]
+    
+type Elements = Set Element
+
+type Link = (Element,Reference,Element)
+
+type Links = Set Link
+
+-- STRICT
+    
+data Model =
+    Model { getElements :: Elements
+          , getLinks    :: Links } deriving (Show,Eq) 
+
+type Match = Element -> Element
+    
+getType :: Element -> Type
+getType = snd
+
+getIdentifier :: Element -> Identifier
+getIdentifier = fst
+         
+matchingPhase :: Match -> Model -> Elements
+matchingPhase mf = (map mf) . getElements
+
+applyPhase :: ReferenceBinding -> Model -> Elements -> Links
+applyPhase rB m es = concat (map (bindingApplication rB m es) es)
+
+bindingApplication :: ReferenceBinding -> Model -> Elements -> Element -> Links
+bindingApplication rB m es targetReferenceSource =
+    let sourceElement = traceLinkTraversal m targetReferenceSource
+        sourceReferenceTargets = rB m sourceElement
+        targetReferenceTargets = concat (map (traceLinkResolution es) sourceReferenceTargets)
+    in map (\tRT -> (targetReferenceSource,R,tRT)) targetReferenceTargets
+
+type ReferenceBinding = Model -> Element -> Elements 
+                                
+traceLinkTraversal :: Model -> Element -> Element
+traceLinkTraversal m (i,_) = eSource
+    where [eSource] = filter ((==i) . getIdentifier) (getElements m) 
+
+traceLinkResolution :: Elements -> Element -> Elements
+traceLinkResolution es (i,_) = filter ((==i) . getIdentifier) es
+
+transformation :: Match -> ReferenceBinding -> Model -> Model
+transformation mf rf m =
+    let es = matchingPhase mf m
+        ls = applyPhase rf m es
+    in Model es ls       
+
+get :: Model -> Element -> Elements
+get m eS = [ eT | (eS',_,eT) <- getLinks m, eS'==eS]
+
+
+-- LAZY STUFF
+       
+data ModelL = ModelL {
+      sourceModel :: Model
+    , matchFunction :: Match
+    , referenceBindingFunction :: ReferenceBinding
+    , validElements :: Elements
+    , targetModel :: Model
+    } 
+
+transformationL :: Match -> ReferenceBinding -> Model -> Element -> ModelL
+transformationL mf rB m sourceOfRoot = ModelL m mf rB [] (Model [targetRoot] [])
+    where targetRoot = mf sourceOfRoot
+                   
+addValidElements :: Element -> ModelL -> ModelL
+addValidElements e lm = lm { validElements = e:validElements lm } 
+
+bindingApplicationL :: ModelL -> Element -> (Elements,Links)
+bindingApplicationL lm targetReferenceSource =
+    let sourceElement = traceLinkTraversal (sourceModel lm) targetReferenceSource
+        sourceReferenceTargets = (referenceBindingFunction lm) (sourceModel lm) sourceElement
+        -- targetReferenceTargets = concat (map (traceLinkResolution es) sourceReferenceTargets) in strict traceLink base on the complete trace
+        targetReferenceTargets = map (matchFunction lm) sourceReferenceTargets
+    in (targetReferenceTargets,map (\tRT -> (targetReferenceSource,R,tRT)) targetReferenceTargets)
+
+getL :: ModelL -> Element -> (Elements, ModelL)
+getL lm e | e `elem` validElements lm = (get (targetModel lm) e,lm)
+          | otherwise
+    = let (es,ls) = bindingApplicationL lm e
+          tm1 = Model (es++getElements tm) (ls++getLinks tm) where tm = targetModel lm
+          lm1 = lm { targetModel = tm1 }
+          lm2 = addValidElements e lm1
+      in (get (targetModel lm2) e,lm2) -- aka (tm1,lm2)
+                        
+
+-- EXAMPLE
+    
+data Type = A | B | C | D deriving (Show,Eq)
+
+data Reference = R deriving (Show,Eq)
+
+m0 :: Model
+m0 = Model [(1,A),(2,B)] [((1,A),R,(2,B))]
+
+myMatch :: Match
+myMatch (i,A) = (i,C)
+myMatch (i,B) = (i,D)
+
+myReferenceBinding :: ReferenceBinding
+myReferenceBinding m e = [ j | (j,R,k) <- getLinks m, k==e ]
+
+m1 :: Model
+m1 = transformation myMatch myReferenceBinding m0
+
+dFS :: Model -> Element -> Elements
+dFS m e = e:concat (map (dFS m) (get m e))
+
+test1 = dFS m1 (2,D)
+     
+lm1 :: ModelL
+lm1 = transformationL myMatch myReferenceBinding m0 (2,B)
+
+dFSLs :: [Element] -> ModelL -> (Elements,ModelL)
+dFSLs []     lm = ([],lm)
+dFSLs (e:es) lm =
+    let (es1,lm1) = getL lm e
+        (es2,lm2) = dFSLs (es++es1) lm1
+    in (e:es2,lm2)
+       
+test1L = fst (dFSLs [(2,D)] lm1)
+
+test = test1==test1L
