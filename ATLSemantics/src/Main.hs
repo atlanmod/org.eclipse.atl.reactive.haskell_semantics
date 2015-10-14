cfe52c4743053c9a836678cefaedbe8d0db81c52
Rémi Douence <douence@dhcp-2-77.rech172-28.emn.fr>
2015-10-14 13:43:20 +0200

    bad version

--- ATLSemantics/src/Main.hs
+++ ATLSemantics/src/Main.hs
@@ -1,11 +1,7 @@
 module Main where
 
-main::IO()
-main = putStrLn (show (transformation m0))
-
-data Type = A | B | C | D deriving (Show,Eq)
-
-data Reference = R | R' deriving (Show,Eq)
+--main::IO()
+--main = putStrLn (show (transformation m0))
 
 type Identifier = Int
               
@@ -19,61 +15,100 @@ type Link = (Element,Reference,Element)
 
 type Links = Set Link
 
-type Model = (Elements,Links)
-
-m0 :: Model
-m0 = ([(1,A),(2,B)],[((1,A),R,(2,B))])
+data Model =
+    Model { getRoot     :: Element
+          , getElements :: Elements
+          , getLinks    :: Links } deriving (Show,Eq) 
 
 type Match = Element -> Element
+    
+getType :: Element -> Type
+getType = snd
 
-match :: Match
-match (i,A) = (i,C)
-match (i,B) = (i,D)
+getIdentifier :: Element -> Identifier
+getIdentifier = fst
+         
+matchingPhase :: Match -> Model -> Elements
+matchingPhase mf = (map mf) . getElements
 
-typeOf :: Element -> Type
-typeOf = snd
+applyPhase :: ReferenceBinding -> Model -> Elements -> Links
+applyPhase rB m es = concat (map (bindingApplication rB m es) es)
 
-matchingPhase :: Model -> Elements
-matchingPhase = (map match) . fst
-
-e1 :: Elements
-e1 = matchingPhase m0
-
-applyPhase :: Model -> Elements -> Links
-applyPhase m es = concat (map (bindingApplication m es) es)
-
-type BindingApplication = Model -> Elements -> Element -> Links
+type BindingApplication = ReferenceBinding -> Model -> Elements -> Element -> Links
 
 bindingApplication :: BindingApplication
-bindingApplication m es e | typeOf e == D = let b = traceLinkTraversal m e
-                                                a = referenceBinding m b
-                                                c = traceLinkResolution es a
-                                            in [(e,R',c)]
-bindingApplication m _  _     = []
+bindingApplication rB m es sE = let sourceElement = traceLinkTraversal m sE
+                                    sourceReferenceTargets = rB m sourceElement
+                                    targetReferenceTargets = concat (map (traceLinkResolution es) sourceReferenceTargets)
+                                in map (\tE -> (sE,R',tE)) targetReferenceTargets
 
-referenceBinding :: Model -> Element -> Element 
-referenceBinding m e = head [ j | (j,R,k) <- snd m, k==e ]
-
+type ReferenceBinding = Model -> Element -> Elements 
+                                
 traceLinkTraversal :: Model -> Element -> Element
-traceLinkTraversal m (i,_) = head (filter ((==i) . fst) (fst m)) 
+traceLinkTraversal m (i,_) = eSource
+    where [eSource] = filter ((==i) . getIdentifier) (getElements m) 
 
-traceLinkResolution :: Elements -> Element -> Element
-traceLinkResolution es (i,_) = head (filter ((==i) . fst) es)
+traceLinkResolution :: Elements -> Element -> Elements
+traceLinkResolution es (i,_) = filter ((==i) . getIdentifier) es
 
-transformation :: Model -> Model
-transformation m =
-    let es = matchingPhase m
-        ls = applyPhase m es
-    in (es,ls)
+transformation :: Match -> ReferenceBinding -> Model -> Model
+transformation mf rf m =
+    let es = matchingPhase mf m
+        ls = applyPhase rf m es
+    in Model (mf (getRoot m)) es ls       
 
+-- Example
+    
+data Type = A | B | C | D deriving (Show,Eq)
+
+data Reference = R | R' deriving (Show,Eq)
+
+m0 :: Model
+m0 = Model (1,A) [(1,A),(2,B)] [((1,A),R,(2,B))]
+
+myMatch :: Match
+myMatch (i,A) = (i,C)
+myMatch (i,B) = (i,D)
+
+myReferenceBinding :: ReferenceBinding
+myReferenceBinding m e = [ j | (j,R,k) <- getLinks m, k==e ]
+
+m1 :: Model
+m1 = transformation myMatch myReferenceBinding m0
+
+     
+     
+-- LAZY STUFF
+       
+data ModelL = ModelL {
+      sourceModel :: Model
+    , matchFunction :: Match
+    , bindingFunction :: BindingApplication
+    , validElements :: Elements
+    , targetModel :: Model
+    } 
+
+transformationL :: Match -> Model -> Element -> ModelL
+transformationL mf m sourceOfRoot = ModelL m mf bindingApplication [targetRoot] (Model targetRoot [targetRoot] [])
+    where targetRoot = mf sourceOfRoot
+                   
+addValidElements :: Element -> ModelL -> ModelL
+addValidElements e lm = lm { validElements = e:validElements lm } 
+{-                                                       
+               
 -- ValidElement means that all outgoing references are valid (in ATL terminology)
-get :: LazyModel -> Element -> (Elements, LazyModel)
-get lm@(m0, match, ba, ves, m1) e | e `elem` ves = ([s | (f,r,s) <- snd m1, f==e], lm)
-                                  | otherwise = get (m0, match, ba, e:ves, (fst m1, snd m1++(ba m0 ves e)) 
+get :: ModelL -> Element -> (Elements, ModelL)
+get lm e | e `elem` validElements lm = ([s | (f,_,s) <- getLinks (sourceModel lm), f==e], lm)
+         | otherwise                 = let lm1 = addValidElement e lm
+                                           
+                                       in get lm1 e
 
-type LazyModel = (Model, Match, BindingApplication, ValidElements, Model)
+                                              , targetModel= Model (getElements (targetModel lm))
+                                                                   (getLinks (targetModel lm)++bindingFunction lm (sourceModel lm) (validElements lm) e)} e 
 
-type ValidElements = Set Element
+
+                                                   {-
+
 
 -- misc
 --referenceBindingApplication :: Model -> Reference -> Element -> Elements
@@ -83,4 +118,5 @@ type ValidElements = Set Element
 --       in () 
 ---    let 
 --applyPhase ::
-    
+    -}
+-}
