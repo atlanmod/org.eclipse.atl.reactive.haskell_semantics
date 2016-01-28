9a8a6847dcf6db7af3fa8e5ccb0b239f2df84bf2
Massimo Tisi <massimo.tisi@mines-nantes.fr>
2016-01-28 18:27:32 +0100

    first complete version!

--- src/Main.hs
+++ src/Main.hs
@@ -15,8 +15,8 @@ module Main where
 import Data.Tuple
 import Data.List
 import Debug.Trace
--- MATHS
 
+-- MATHS
 type SetOf a = [a]
 
 --distinguish relation and function? and bijection?
@@ -32,12 +32,6 @@ inverseImage = image . map swap
 
 crossProduct :: Element -> SetOf Element -> Relation
 crossProduct e1 es2 = [ (e1,e2) | e2 <- es2 ]
-
---fixedPoint :: Eq a => ((a,state) -> (a,state)) -> (a, state) -> [a],
---fixedPoint f a | f a == a  = [a]
---               | otherwise = a:fixedPoint f (f a)
-
--- MODELS
 
 type Link = (Element,Element)
 
@@ -64,8 +58,7 @@ getN n m = let (es', m') = getN (n-1) m
                (es'', m'') = getAllFromTarget m' es'
            in (union es' es'', m'')
 
--- data ModelStrict = ModelStrict Model deriving Show
-data TransformationStrict = TransformationStrict (Model,Transformation,Model) deriving Show
+newtype TransformationStrict = TransformationStrict (Model,Transformation,Model) deriving Show
 instance TransformationI TransformationStrict where
     getRootFromTarget (TransformationStrict (_,_,(root,_,_))) = root
     getFromTarget m@(TransformationStrict (_,_,(_,_,links))) e = (image links [e],m)
@@ -76,7 +69,16 @@ instance TransformationI TransformationStrict where
             targetLinks = applyPhase t m targetElements
         in TransformationStrict (m, t, (targetRoot,targetElements,targetLinks))
 
-data TransformationLazy = TransformationLazy (Model,Transformation,Model) deriving Show
+
+-- model strict transformation
+-- it obtains the transformed root and all transformed elements
+matchingPhase :: Transformation -> Model -> (Element,SetOf Element)
+matchingPhase t (root,elements,_) = (head (image t [root]),image t elements)
+
+applyPhase :: Transformation -> Model -> SetOf Element -> SetOf Link
+applyPhase t m = concatMap (bindingApplication t m)
+
+newtype TransformationLazy = TransformationLazy (Model,Transformation,Model) deriving Show
 instance TransformationI TransformationLazy where
     getRootFromTarget (TransformationLazy (_,_,(rootT,_,_))) = rootT
     getFromTarget mL@(TransformationLazy (mS,t,mT@(rootT,elementsT,linksT))) e | e `elem` elementsT = (image linksT [e],mL)
@@ -93,11 +95,7 @@ initialize t m = (m,t,(rootT,[],[]))
      where (root,_,_) = m
            [rootT] = image t [root]
 
---instance TransformedModelI ModelLazy where
---    addElementToSource e m = undefined
---    addLinkToSource l m = undefined
-
-data TransformationIncremental = TransformationIncremental (Model, Transformation, Model) deriving Show
+newtype TransformationIncremental = TransformationIncremental (Model, Transformation, Model) deriving Show
 instance TransformationI TransformationIncremental where
     getRootFromTarget(TransformationIncremental (_,_,(rootT,_,_))) = rootT
     getFromTarget m@(TransformationIncremental (_, _, (_,_,links))) e = (image links [e],m)
@@ -106,118 +104,38 @@ instance TransformationI TransformationIncremental where
                                    in TransformationIncremental ((root,e:es,links), t, (root',ne:es',links'))
     addLinkToSource (from, to) (TransformationIncremental ((root,es,links), t, (root',es',links'))) =
                                    let msu = (root,es,(from, to):links)
-                                       ls = bindingApplication t msu (head (image t [to]))
+                                       ls = bindingApplication t msu (head (image t [from]))
                                    in TransformationIncremental (msu, t, (root',es',ls++links'))
     apply (TransformationIncremental (m, t, _))  =
             let (targetRoot,targetElements) = matchingPhase t m
                 targetLinks = applyPhase t m targetElements
             in TransformationIncremental (m, t, (targetRoot,targetElements,targetLinks))
 
-type Transformation = Relation
 
--- sourceModel -> matchedElements -> resultingSourceElements
-computeBinding :: Model -> SetOf Element -> SetOf Element
-computeBindingTraces :: Model -> SetOf Element -> SetOf Element
+newtype TransformationReactive = TransformationReactive (Model, Transformation, Model) deriving Show
+instance TransformationI TransformationReactive where
+    getRootFromTarget(TransformationReactive (_,_,(rootT,_,_))) = rootT
+    getFromTarget mL@(TransformationReactive (mS,t,mT@(rootT,elementsT,linksT))) e | e `elem` elementsT = (image linksT [e],mL)
+                                                                                   | otherwise
+                 = let ls = bindingApplication t mS e
+                       mT1 = (rootT,e:elementsT,ls++linksT)
+                   in (image (ls++linksT) [e],TransformationReactive (mS, t, mT1))
+    addElementToSource e (TransformationReactive ((root,es,links), t, (root',es',links'))) =
+                                   TransformationReactive ((root,e:es,links), t, (root',es',links'))
+    addLinkToSource (from, to) (TransformationReactive ((root,es,links), t, (root',es',links'))) =
+                                   let msu = (root,es,(from, to):links)
+                                       [ne] = image t [from]
+                                   in TransformationReactive (msu, t, (root',delete ne es',links'))
+    apply (TransformationReactive (m,t,_)) = TransformationReactive (initialize t m)
+
+type Transformation = Relation
 
 -- we resolve every time because we don't compute primitive attributes and elements are created by different rules
 -- we compute the full binding (all the links)
 -- transformation -> transformationSourceModel -> targetLinkSource -> targetLinks
 bindingApplication :: Transformation -> Model -> Element -> SetOf Link
-bindingApplication t m@(_,_,links) targetLinkSource = trace (show t ++ show m ++ show targetLinkSource ++ show (inverseImage t [targetLinkSource])) $
+bindingApplication t m@(_,_,links) targetLinkSource = -- trace (show t ++ show m ++ show targetLinkSource ++ show (inverseImage t [targetLinkSource])) $
     targetLinkSource `crossProduct` (image t . computeBinding m . inverseImage t) [targetLinkSource]
-
--- type Transformation a = a -> a
--- STRICT
-
--- model strict transformation
--- it obtains the transformed root and all transformed elements
-matchingPhase :: Transformation -> Model -> (Element,SetOf Element)
-matchingPhase t (root,elements,_) = (head (image t [root]),image t elements)
-
-applyPhase :: Transformation -> Model -> SetOf Element -> SetOf Link
-applyPhase t m = concatMap (bindingApplication t m)
-
---transformationStrict :: Transformation -> ModelStrict -> ModelStrict
---transformationStrict t (ModelStrict m)  =
---    let (targetRoot,targetElements) = matchingPhase t m
---        targetLinks = applyPhase t m targetElements
---    in ModelStrict (targetRoot,targetElements,targetLinks)
-
---transformationLazy :: Transformation -> ModelStrict -> ModelLazy
---transformationLazy t (ModelStrict m) = ModelLazy (initialize t m)
-
--- call a 'get' (i.e. navigate) on all the links outgoing from all the elements in the set
--- get :: (SetOf Element,Model) -> (SetOf Element,Model)
--- get (es,m@(_,_,links)) = (image links es,m)
-
--- Adds (strictly) element/link to Model
---addElementS :: Element -> Model -> Model
---addElementS e (rootS,elementsS,linksS) = (rootS,e:elementsS,linksS)
-
---addLinkS :: Link -> Model -> Model
---addLinkS l (rootS,elementsS,linksS) = (rootS,elementsS,l:linksS)
-
--- LAZY
-
--- type ModelL = (Model,Transformation,Model)
-
--- initialize :: Transformation -> Model -> (Model,Transformation,Model)
---initialize t m = (m,t,(rootT,[],[]))
---     where (root,_,_) = m
---           [rootT] = image t [root]
-
--- call a 'get' (i.e. navigate) lazily on all the links outgoing from one element, by computing the element if necessary
--- get1L :: ModelL -> Element -> (SetOf Element, ModelL)
--- get1L mL@(mS,t,mT@(rootT,elementsT,linksT)) e | e `elem` elementsT = (fst (get ([e],mT)),mL)
---                                               | otherwise
---     = let ls = bindingApplication t mS e
---           mT1 = (rootT,e:elementsT,ls++linksT)
---           mL2 = (mS,t,mT1)
---       in (fst (get ([e],mT1)),mL2)
---
--- -- get1L on a set of elements
--- getL :: (SetOf Element,ModelL) -> (SetOf Element, ModelL)
--- getL (  [],mL0) = ([],mL0)
--- getL (e:es,mL0) = let (est1,mL1) = get1L mL0 e
---                       (est2,mL2) = getL (es,mL1)
---                   in (est1 `union` est2,mL2)
-
--- INCREMENTAL
--- Adds element/link to Source Model (and updates the corresponding target model)
---addElementU :: Element -> (Model, Transformation, Model) -> (Model, Transformation, Model)
---addElementU e (ms, t, mt) =
---         let [ne] = image t [e]
---         in (addElementS e ms, t, addElementS ne mt)
---
--- -- this works only in our case where we just invert links
--- addLinkU :: Link -> (Model, Transformation, Model) -> (Model, Transformation, Model)
--- addLinkU (from, to) (ms, t, mt) =
---         let msu = addLinkS (from, to) ms
---             ls = bindingApplication t msu (head (image t [to]))
---         in (msu, t, foldr addLinkS mt ls)
-
--- REACTIVE
-
---get1R :: ModelL -> Element -> (SetOf Element, ModelL)
-
---getR :: (SetOf Element,ModelL) -> (SetOf Element, ModelL)
-
--- nothing happens on the target
--- addElementR :: Element -> (Model,Transformation,Model) -> (Model,Transformation,Model)
--- addElementR e (ms, t, mt) =
---     (addElementS e ms, t, mt)
---
--- addLinkR :: Link -> (Model,Transformation,Model) -> (Model,Transformation,Model)
--- addLinkR (from, to) (ms, t, mt) =
---     let msu = addLinkS (from, to) ms
---     in undefined -- (msu, t, )
-
--- isInvolvedIn :: Element
-
-
-
-
-
 
 -- EXAMPLE
 
@@ -234,8 +152,14 @@ tl0 :: TransformationLazy
 tl0 = TransformationLazy ((A,[A,B],[(A,B)]), tr, undefined)
 ti0 :: TransformationIncremental
 ti0 = TransformationIncremental ((A,[A,B],[(A,B)]), tr, undefined)
+tr0 :: TransformationReactive
+tr0 = TransformationReactive ((A,[A,B],[(A,B)]), tr, undefined)
 
 -- Transformation
+
+-- sourceModel -> matchedElements -> resultingSourceElements
+computeBinding :: Model -> SetOf Element -> SetOf Element
+computeBindingTraces :: Model -> SetOf Element -> SetOf Element
 
 -- computeBinding (_,_,links) = inverseImage links
 computeBinding (_,_,links) = image links
@@ -244,27 +168,8 @@ computeBindingTraces (_,_,links) elements = nub (elements ++ image links element
 traces :: Transformation -> Element -> SetOf Element
 traces t e = image t [e]
 
---test:: TransformationI m => TransformationStrict -> (SetOf Element, m)
---test ts = getN 3 (apply ts)
-
 updateSource :: TransformationI m => m -> m
 updateSource ts = addLinkToSource (A, E) (addElementToSource E ts)
-
---update' :: TransformationStrict -> TransformationStrict
---update' ts = addLinkToSource (A, E) (addElementToSource E ts)
-{-
-test3 :: [SetOf Element]
-test3 = map fst (traversal ([root],mtu))
-        where (root,_,_) = mtu
-
-mtui :: Model
-mtui = mtui1
-        where (_,_,mtui1) = addLinkU (E,A) (addElementU E (m0, t2, m1))
-
-test4 :: [SetOf Element]
-test4 = map fst (traversal ([root],mtui))
-        where (root,_,_) = mtui
--}
 
 mStrict :: TransformationStrict
 mStrict = apply ts0
@@ -272,6 +177,8 @@ mLazy :: TransformationLazy
 mLazy = apply tl0
 mIncremental :: TransformationIncremental
 mIncremental = apply ti0
+mReactive :: TransformationReactive
+mReactive = apply tr0
 
 mStrict' :: TransformationStrict
 mStrict' = apply $ updateSource $ apply ts0
@@ -279,41 +186,17 @@ mLazy' :: TransformationLazy
 mLazy' = apply $ updateSource $ apply tl0
 mIncremental' :: TransformationIncremental
 mIncremental' = updateSource $ apply ti0
+mReactive' :: TransformationReactive
+mReactive' = updateSource $ apply tr0
 
 main::IO()
 main = do
---    print $ show (fst ((test m0) :: (SetOf Element, ModelStrict)))
---    print $ show (fst ((test m0) :: (SetOf Element, ModelLazy)))
---    print $ show (fst ((test m0) :: (SetOf Element, ModelIncremental)))
     print $ show $ fst $ getN 3 mStrict
     print $ show $ fst $ getN 3 mLazy
     print $ show $ fst $ getN 3 mIncremental
+    print $ show $ fst $ getN 3 mReactive
 
     print $ show $ fst $ getN 3 mStrict'
     print $ show $ fst $ getN 3 mLazy'
-    print $ show $ mIncremental'
     print $ show $ fst $ getN 3 mIncremental'
-    print $ show $ getN 3 mIncremental'
-
-
---main::IO()
---main = print "ciao"
-
--- data Element = A | B | C | D | E | F deriving (Show,Eq) -- distinguish source and target element types?
--- type Transformation = Relation
---
--- matchingPhase :: Transformation -> Model -> (Element,SetOf Element)
--- matchingPhase t (root,elements,_) = (head (image t [root]),image t elements)
---
--- applyPhase :: Transformation -> Model -> SetOf Element -> SetOf Link
--- applyPhase t m = concatMap (bindingApplication t m)
---
--- bindingApplication :: Transformation -> Model -> Element -> SetOf Link
--- bindingApplication t m@(_,_,links) targetLinkSource =
---     targetLinkSource `crossProduct` (image t . computeBinding m . inverseImage t) [targetLinkSource]
---
--- computeBinding :: Model -> SetOf Element -> SetOf Element
--- computeBindingTraces :: Model -> SetOf Element -> SetOf Element
---
--- computeBinding (_,_,links) = image links
--- computeBindingTraces (_,_,links) elements = nub (elements ++ image links elements)
+    print $ show $ fst $ getN 3 mReactive'
