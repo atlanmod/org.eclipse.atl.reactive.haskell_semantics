9bf1ada33833d70523ff7bdd9ceea37f9538eae3
Massimo Tisi <massimo.tisi@mines-nantes.fr>
2016-02-16 14:32:19 +0100

    QuickCheck with random sequences of updates

--- src/Main.hs
+++ src/Main.hs
@@ -40,8 +40,8 @@ type Link = (Element,Element)
 -- in lazy semantics (root, all elements with valid outgoing references, all links from valid elements)
 type Model = (Element,SetOf Element,SetOf Link)
 
-class TransformationI m where
-    apply :: m -> m
+class TransformationI m where  -- MONADIC (State) ?
+    apply :: m -> m -- should be implicit in the constructor ?
     getFromTarget :: m -> Element -> (SetOf Element,m)
     getRootFromTarget :: m -> Element
     addElementToSource :: Element -> m -> m
@@ -199,7 +199,9 @@ main = do
     print $ show $ fst $ getN 3 mIncremental'
     print $ show $ fst $ getN 3 mReactive'
 
-    test1
+    test'
+
+    -- print $ show $ bug
 
 -- Quicheck generation of source models and transformations
 
@@ -236,7 +238,7 @@ newtype Config = Config { unConfig::(Model,Transformation) } deriving (Show,Eq)
 configs :: Gen Config
 configs = do
   -- take a subset of at leat two elements and split it in two
-  (elementS,elementT) <- elements $ concat $ map split $ filter (\es -> length es>=2) $ subSet [A .. F]
+  (elementS,elementT) <- elements $ sourceAndTargetElements
   -- a subset of all possible source links
   linkS <- elements $ subSet $ allLinks elementS
   let rootS = head elementS -- source root is always the first element
@@ -265,3 +267,80 @@ test2 = verboseCheck assertion
 valid :: Model -> Transformation -> Bool
 valid (root, es, links) t = elem root es && length es == length (nub es) && all (`elem` es) (map fst links) && all (`elem` es) (map snd links) &&
                             all (`notElem` es) (map snd t) && not (null t) && root `elem` (map fst t)
+
+
+-- Quickchecking updates
+-- updateSource ts = addLinkToSource (A, E) (addElementToSource E ts)
+
+-- delete an element but root, and return this element and its links
+deleteAnElement :: Element -> Model -> (Model,[Instruction])
+deleteAnElement e (root,es,links) = ((root, filter (/=e) es, filter (not . (involving e)) links),AddElement e:map AddLink (filter (involving e) links)) 
+    where involving :: Element -> Link -> Bool
+          involving e (from,to) = e==from || e==to
+
+deleteElements :: [Element] -> Model -> (Model,[Instruction]) -- MONADIC 
+deleteElements []     m = (m,[])
+deleteElements (e:es) m = let (m1,is1) = deleteAnElement e m
+                              (m2,is2) = deleteElements es m1
+                          in (m2,is1++is2) -- beware of the order l2r or r2l of instructions
+                                  
+data Instruction = AddElement Element | AddLink Link deriving (Show,Eq)
+
+--delayI :: (Instruction,Instruction) -> [(Instruction,Instruction)]
+--delayI (AddLink    l,i) = [(AddLink    l,i),(i,AddLink l)]
+--delayI (AddElement e,i) = [(AddElement e,i)]
+          
+permutI :: [Instruction] -> [[Instruction]]
+permutI (AddElement e:is) = map (AddElement e:) (permutI is) -- AddElement cannot be delayed
+permutI (AddLink    l:is) = concat (map (insertI (AddLink l)) (permutI is))
+permutI []                = [[]]
+
+insertI :: Instruction -> [Instruction] -> [[Instruction]]
+insertI i0 [] = [[i0]]
+insertI i0 (i:is) = (i0:i:is) : map (i:) (insertI i0 is)
+
+runI :: TransformationI m => [Instruction] -> m -> m
+runI []                m = m
+runI (AddElement e:is) m = runI is (addElementToSource e m) 
+runI (AddLink    l:is) m = runI is (addLinkToSource l m)
+
+
+
+newtype Config' = Config' { unConfig'::([Instruction],Model,Transformation) } deriving (Show,Eq)
+
+configs' :: Gen Config'
+configs' = do
+  -- take a subset of at leat two elements and split it in two
+  (elementS,elementT) <- elements $ concat $ map split $ filter (\es -> length es>=2) $ subSet [A .. F]
+  -- a subset of all possible source links
+  linkS <- elements $ subSet $ allLinks elementS
+  let rootS = head elementS -- source root is always the first element
+      rootT = head elementT -- target root is always the first element
+  -- a subset of all possible transfo (does not include root mapping)
+  transfo <- elements $ subSet $ allTransfos (tail elementS) elementT
+  -- a subset of elements (but root)
+  elementsToBeDeleted <- elements $ subSet $ elementS \\ [rootS]
+  -- delete them
+  let (mStripped,is) = deleteElements elementsToBeDeleted (rootS,elementS,linkS)
+  -- and generate a sequence of instructions to restore them
+  instructions <- elements $ permutI is
+  -- a configuration
+  return $ Config' (instructions,mStripped,(rootS,rootT):transfo)
+
+instance Arbitrary Config' where
+    arbitrary = configs'
+                           
+test' = verboseCheckWith stdArgs { maxSuccess = 1000 } assertion'
+
+assertion' config =
+    let (is,mS,t) = unConfig' config
+        c = (mS,t,undefined)
+    in --valid mS t ==>
+       length (nub [ fst $ getN 3 $ apply $ runI is $ apply $ TransformationStrict c
+                   , fst $ getN 3 $ apply $ runI is $ apply $ TransformationLazy c
+                   , fst $ getN 3 $ runI is $ apply $ TransformationIncremental c
+                   , fst $ getN 3 $ runI is $ apply $ TransformationReactive c]) == 1
+
+-- ([AddElement C,AddLink (A,C)],(A,[A,B],[(A,A),(A,B),(B,B)]),[(A,D),(C,D),(C,F)])
+
+-- bug = fst $ getN 3 $ apply $ runI [AddElement C,AddLink (A,C)] $ apply $ TransformationReactive ((A,[A,B],[(A,A),(A,B),(B,B)]),[(A,D),(C,D),(C,F)], undefined)
