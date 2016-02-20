bdd22e224b08337218de30d476677b7d7772ec4d
Massimo Tisi <massimo.tisi@mines-nantes.fr>
2016-02-20 09:37:21 +0100

    Cleaned up

--- src/Main.hs
+++ src/Main.hs
@@ -1,18 +1,7 @@
--- Abstract (semantics-like) version
--- Restriction ReactiveATL w.r.t. standard ATL
--- * allInstances is not Reactive
--- * inverse (that is implemented by allInstances) is not reactive either
---
--- Simplifications w.r.t. ReactiveATL:
--- * no attribute
--- * only one reference for each metaelement
--- * no multiple output pattern elements
--- * all references have the same binding
--- * multiple input pattern elements are encoded as couples (not a restriction)
+-- see Semantics.hs for limitations
 
 module Main where
 
-import Semantics
 import Example
 import QuickChecker
 
